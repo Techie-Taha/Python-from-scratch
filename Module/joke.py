dd06b94d26d758da93a6599b98e624c7fef72915
Taha <ta753090@gmail.com>
2025-08-21 18:51:56 -0400

    File added

--- Module/joke.py
+++ Module/joke.py
@@ -1,4 +1,4 @@
 # we installed pyjokes by typing pip install pyjokes in the terminal. just do import pyjokes
-import pyjokes
-joke = pyjokes.get_joke()
+import pyjokes 
+joke = pyjokes.get_joke() # get_joke(), a fixed function from the pyjokes module is called and its result is stored in the variable joke
 print(joke)
